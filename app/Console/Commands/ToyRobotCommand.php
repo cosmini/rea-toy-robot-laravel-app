e48f1a42dbb0884c29ce8d78722617b64cb9ecab
Anonymous <candidate@example.com>
2019-11-04 10:28:49 +1100

    [wip] Adding commands for robot and other needed data.

--- app/Console/Commands/ToyRobotCommand.php
+++ app/Console/Commands/ToyRobotCommand.php
@@ -21,6 +21,48 @@ class ToyRobotCommand extends Command
     protected $description = 'Start simulation of a toy robot moving on a square tabletop. Valid input commands are PLACE X,Y,F; MOVE; LEFT, RIGHT, REPORT.';
 
     /**
+     * The mapping of cardinal points and their value
+     *
+     * @var array
+     */
+    private $cardinalPoints = [
+        'EAST' => 0,
+        'NORTH' => 1,
+        'WEST' => 2,
+        'SOUTH' => 3,
+    ];
+
+    /**
+     * Accepted commands by the robot
+     *
+     * @var array
+     */
+    private $validRobotCommands = [
+        'PLACE',
+        'MOVE',
+        'LEFT',
+        'RIGHT',
+        'REPORT',
+    ];
+
+    /**
+     * Valid commands to exit the simulator
+     *
+     * @var array
+     */
+    private $validExitCommands = [
+        'EXIT',
+        'QUIT',
+    ];
+
+    /**
+     * If robot is placed on the table or not
+     *
+     * @var bool
+     */
+    private $isPlaced = false;
+
+    /**
      * Create a new command instance.
      *
      * @return void
