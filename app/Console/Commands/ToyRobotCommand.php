fffdf9a12c92789ae9b5182dc876e52107e9799c
Anonymous <candidate@example.com>
2019-11-05 09:35:28 +1100

    Removed valid commands since they are covered by the regex in validation method. Implemented method for turning left or right. Implemented method that will action any command.

--- app/Console/Commands/ToyRobotCommand.php
+++ app/Console/Commands/ToyRobotCommand.php
@@ -33,19 +33,6 @@ class ToyRobotCommand extends Command
     ];
 
     /**
-     * Accepted commands by the robot
-     *
-     * @var array
-     */
-    private $validRobotCommands = [
-        'PLACE',
-        'MOVE',
-        'LEFT',
-        'RIGHT',
-        'REPORT',
-    ];
-
-    /**
      * Valid commands to exit the simulator
      *
      * @var array
@@ -72,7 +59,7 @@ class ToyRobotCommand extends Command
     /**
      * Value of direction, any of the cardinal points
      *
-     * @var integer
+     * @var string
      */
     private $direction;
 
@@ -97,7 +84,22 @@ class ToyRobotCommand extends Command
             $input = rtrim(trim(fgets(STDIN)));
             if ($this->isExitCommand($input)) {
                 $this->info('Closing robot simulator. Bye!');
-                die(0);
+                break;
+            }
+
+            if (!$this->isValidRobotCommand($input)) {
+                $this->warn('Invalid command. Allowed options are: PLACE, MOVE, LEFT, RIGHT and REPORT');
+                continue;
+            }
+
+            if (!$this->isRobotPlacedOnTable()) {
+                $this->info('To start the simulator place the robot on the table using PLACE X,Y,FACING');
+                $this->info('Example: PLACE 2,3,EAST');
+                continue;
+            }
+
+            if (!$this->engageCommand($input)) {
+                $this->warn('Command ignored.');
             }
         }
     }
@@ -107,7 +109,7 @@ class ToyRobotCommand extends Command
      *
      * @return bool
      */
-    public function getIsPlaced()
+    public function isRobotPlacedOnTable()
     {
         return isset($this->coordinateX, $this->coordinateY, $this->direction);
     }
@@ -135,5 +137,75 @@ class ToyRobotCommand extends Command
     public function isValidRobotCommand($input)
     {
         return preg_match('/^(PLACE [0-5],[0-5],(EAST|WEST|NORTH|SOUTH)|MOVE|LEFT|RIGHT|REPORT)$/', $input) === 1;
+    }
+
+    /**
+     * Get robot direction after left or right commands
+     *
+     * @param string $turn
+     *
+     * @return string
+     */
+    public function turnRobot(string $turn)
+    {
+        $turnShift = $turn === 'LEFT' ? 1 : -1;
+        $directionValue = $this->cardinalPoints[$this->direction];
+
+        $newDirection = $directionValue + $turnShift;
+
+        if ($newDirection < 0) {
+            return array_flip($this->cardinalPoints)[3];
+        } else if ($newDirection > 3) {
+            return array_flip($this->cardinalPoints)[0];
+        }
+
+        return array_flip($this->cardinalPoints)[$newDirection];
+    }
+
+    /**
+     * Take action from command
+     *
+     * @param string $input
+     *
+     * @return bool
+     */
+    public function engageCommand($input)
+    {
+        $success = false;
+
+        switch ($input) {
+            case starts_with($input, 'PLACE'):
+                $placement = explode(',', str_replace('PLACE ', '', $input));
+
+                $this->coordinateX = $placement[0];
+                $this->coordinateY = $placement[1];
+                $this->direction = $placement[2];
+
+                $success = true;
+                break;
+
+            case $input === 'MOVE':
+                // TODO: implement move command including validation to stay on the table
+                break;
+
+            case $input === 'LEFT':
+            case $input === 'RIGHT':
+                $this->direction = $this->turnRobot($input);
+                $success = true;
+                break;
+
+            case $input === 'REPORT':
+                $this->info(
+                    implode(',', [
+                        $this->coordinateX,
+                        $this->coordinateY,
+                        $this->direction
+                    ])
+                );
+                $success = true;
+                break;
+        }
+
+        return $success;
     }
 }
