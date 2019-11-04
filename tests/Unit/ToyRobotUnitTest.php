afb8ab6f1ad7dfb8f57e6bca27b9aa66a3f2e998
Anonymous <candidate@example.com>
2019-11-04 17:07:25 +1100

    Added test coverage comments. Initialised unit test for robot commands validation.

--- tests/Unit/ToyRobotUnitTest.php
+++ tests/Unit/ToyRobotUnitTest.php
@@ -16,15 +16,29 @@ class ToyRobotUnitTest extends TestCase
         $this->robotCommand = new ToyRobotCommand();
     }
 
+    /**
+     * @covers App\Console\Commands\ToyRobotCommand::getIsPlaced
+     */
     public function testIsPlacedAttributeGetter()
     {
         $this->assertFalse($this->robotCommand->getIsPlaced());
     }
 
+    /**
+     * @covers App\Console\Commands\ToyRobotCommand::isExitCommand
+     */
     public function testIsExitCommandMethod()
     {
         $this->assertTrue($this->robotCommand->isExitCommand('EXIT'));
         $this->assertTrue($this->robotCommand->isExitCommand('QUIT'));
         $this->assertFalse($this->robotCommand->isExitCommand('STOP'));
+    }
+
+    /**
+     * @covers App\Console\Commands\ToyRobotCommand::isValidRobotCommand
+     */
+    public function testItCanValidateRobotCommands()
+    {
+        $this->assertFalse($this->robotCommand->isValidRobotCommand(''));
     }
 }
